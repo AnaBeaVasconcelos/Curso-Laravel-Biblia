4d3bb3dbeecdacb088bd2926c00d889e8252d938
AnaBeaVasconcelos <bihhtuubee@gmail.com>
2022-11-20 23:49:56 -0300

    aprendendo mais sobre as rotas

--- routes/api.php
+++ routes/api.php
@@ -21,13 +21,12 @@ Route::get('/teste', function () {
     return "Teste com sucesso!";
 });
 
+// Criação das rotas
 // Route::get('/testamento', [TestamentoController::class, 'index']);
 // Route::get('/testamento/{id}', [TestamentoController::class, 'show']);
 // Route::put('/testamento/{id}', [TestamentoController::class, 'update']);
 // Route::post('/testamento', [TestamentoController::class, 'store']);
 // Route::delete('/testamento/{id}', [TestamentoController::class, 'destroy']);
-
-Route::apiResource('testamento', TestamentoController::class);
 
 // Route::get('/livro', [LivroController::class, 'index']);
 // Route::get('/livro/{id}', [LivroController::class, 'show']);
@@ -35,15 +34,24 @@ Route::apiResource('testamento', TestamentoController::class);
 // Route::post('/livro', [LivroController::class, 'store']);
 // Route::delete('/livro/{id}', [LivroController::class, 'destroy']);
 
-Route::apiResource('livro', LivroController::class);
-
 // Route::get('/versiculo', [VersiculoController::class, 'index']);
 // Route::get('/versiculo/{id}', [VersiculoController::class, 'show']);
 // Route::put('/versiculo/{id}', [VersiculoController::class, 'update']);
 // Route::post('/versiculo', [VersiculoController::class, 'store']);
 // Route::delete('/versiculo/{id}', [VersiculoController::class, 'destroy']);
 
-Route::apiResource('versiculo', VersiculoController::class);
+
+// Aprimorando as rotas 1/2
+// Route::apiResource('livro', LivroController::class);
+// Route::apiResource('testamento', TestamentoController::class);
+// Route::apiResource('versiculo', VersiculoController::class);
+
+// Aprimorando as rotas 2/2
+Route::apiResource([
+    'testamento' =>  TestamentoController::class,
+    'livro'=> LivroController::class,
+    'versiculo' => VersiculoController::class,
+]);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
